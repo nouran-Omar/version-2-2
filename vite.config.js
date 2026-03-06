faf030c4a710cf196eae04e20f0e7bf5ecc88d0d
nouran-Omar <nooromro63.gmail.com>
2026-03-06 20:11:37 +0200

    Fix: update vite config and increase chunk size limit

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import flowbiteReact from "flowbite-react/plugin/vite"
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
     flowbiteReact()
   ],
-esbuild: {
+  esbuild: {
     drop: ['console', 'debugger'],
   },
+  build: {
+    // السطر ده هيخفي التحذير بتاع الـ Chunks الكبيرة اللي ظهرلك في الـ Terminal
+    chunkSizeWarningLimit: 1600, 
+  }
 })
